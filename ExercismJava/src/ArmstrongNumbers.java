b9a7be101f931003c2c64f02e8707b327df93afc
Fabio <fabioescobarardila@hotmail.com>
2024-01-11 14:51:32 -0500

    Exersism: 32. ArmstrongNumbers Exercise completed

---
+++ ExercismJava/src/ArmstrongNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class ArmstrongNumbers {
+
+    boolean isArmstrongNumber(int numberToCheck) {
+        int digits = numberToCheck == 0 ? 1 : (int) Math.log10(numberToCheck) +1;
+        int result = Integer.toString(numberToCheck)
+                .chars()
+                .map(ch -> (int)Math.pow(ch-48, digits))
+                .peek(System.out::println)
+                .reduce((sum, n) -> sum += n)
+                .getAsInt();
+        return result == numberToCheck;
+    }
+
+    public static void main(String[] args) {
+        ArmstrongNumbers an = new ArmstrongNumbers();
+        System.out.println(an.isArmstrongNumber(0));
+    }
+
+}
