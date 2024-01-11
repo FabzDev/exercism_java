99f238ad0afb61fa73264f2ab1247f118e0575ee
Fabio <fabioescobarardila@hotmail.com>
2024-01-10 23:29:35 -0500

    Exersism: 32. Leap Exercise completed

---
+++ ExercismJava/src/Leap.java
@@ -0,0 +1,17 @@
+public class Leap {
+    boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// return year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
